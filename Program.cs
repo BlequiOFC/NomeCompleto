da36007e71c927bc522a8b9671cdbe89ef325459
Bléqui <gustavopregigueiro6@gmail.com>
2023-04-01 14:19:31 -0300

    Adição do código

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+
+Console.WriteLine("Digite o seu primeiro nome.");
+nome = Console.ReadLine()!;
+
+Console.WriteLine("Digite o seu sobrenome.");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Seu nome completo é {nome} {sobrenome}");
