f2ca2ab53603b7b20c1cd066ff792a80c766f011
Bléqui <gustavopregigueiro6@gmail.com>
2023-04-01 14:38:57 -0300

    Adição do nome de catálogo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿string nome, sobrenome;
+﻿Console.Clear();
+string nome, sobrenome;
 
 Console.WriteLine("Digite o seu primeiro nome.");
 nome = Console.ReadLine()!;
@@ -7,3 +8,4 @@ Console.WriteLine("Digite o seu sobrenome.");
 sobrenome = Console.ReadLine()!;
 
 Console.WriteLine($"Seu nome completo é {nome} {sobrenome}");
+Console.WriteLine($"Seu nome de catálogo é {sobrenome.ToUpper()} {nome}");
